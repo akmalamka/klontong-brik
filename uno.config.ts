3f190fe06f9f44e23b93eb860f777244f3a13504
Muhammad Akmal <akmalmuhammad51@gmail.com>
2025-10-14 17:55:41 +0700

    feat: add preset wind3

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,8 @@
+import presetWind3 from '@unocss/preset-wind3'
 import { defineConfig } from 'unocss'
 
 export default defineConfig({
-  // ...UnoCSS options
+  presets: [
+    presetWind3(),
+  ],
 })
