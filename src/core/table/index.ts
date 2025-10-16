380142c4711ad272bbd6168ac41f2a30a16797cf
Muhammad Akmal <akmalmuhammad51@gmail.com>
2025-10-16 20:01:19 +0700

    refactor: remove unnecessary component

--- src/core/table/index.ts
+++ src/core/table/index.ts
@@ -1,8 +1,6 @@
 import CoreTable from './CoreTable.vue'
 import CoreTableBody from './CoreTableBody.vue'
-import CoreTableCaption from './CoreTableCaption.vue'
 import CoreTableCell from './CoreTableCell.vue'
-import CoreTableFooter from './CoreTableFooter.vue'
 import CoreTableHead from './CoreTableHead.vue'
 import CoreTableHeader from './CoreTableHeader.vue'
 import CoreTableRow from './CoreTableRow.vue'
@@ -10,9 +8,7 @@ import CoreTableRow from './CoreTableRow.vue'
 export {
   CoreTable,
   CoreTableBody,
-  CoreTableCaption,
   CoreTableCell,
-  CoreTableFooter,
   CoreTableHead,
   CoreTableHeader,
   CoreTableRow,
